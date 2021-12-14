5e86ee062ef4340b51387a1595631ed0d0ba31a1
Rheygine Medel <rmedel@codeaurora.org>
2021-12-14 15:59:46 +0000

    RM6785: prop: Disable blur on app-launch
    * This change adds the system property that disables blur during app launches.
    
    Signed-off-by: UdayRocker <udaycoc40@gmail.com>

--- properties.mk
+++ properties.mk
@@ -27,3 +27,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
+
+# Blur on app-launch
+ro.launcher.blur.appLaunch=0
