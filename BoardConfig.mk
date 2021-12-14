c5e15b41ce7124dfcbbd0b27e92c7a4f49013b10
Arian <arian.kulmer@web.de>
2021-12-14 15:46:32 +0000

    RM6785: prop: Enable support of one-handed mode for phone targets
    Signed-off-by: UdayRocker <udaycoc40@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,7 @@ BOARD_MAIN_PARTITION_LIST := system product vendor odm system_ext
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6785
